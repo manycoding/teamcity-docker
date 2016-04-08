2b8f9519b9afd86fc3aa8bb383f1695995c43f13
manycoding <vamukhs@gmail.com>
2016-04-08 16:11:34 +0300

    Added buildAgent and a plugin for auto auth

--- Dockerfile
+++ Dockerfile
@@ -7,15 +7,19 @@ ENV TEAMCITY_DATA_PATH /var/lib/teamcity
 
 # Install docker
 RUN apt-get update && apt-get install -y --no-install-recommends \
-    docker \
-    && apt-get clean autoclean \
-    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+    docker && \
+    apt-get clean autoclean && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 # Get and install teamcity
 RUN wget -qO- https://download.jetbrains.com/teamcity/TeamCity-$TEAMCITY_VERSION.tar.gz | tar xz -C /opt
 
 # Enable the correct Valve when running behind a proxy
 RUN sed -i -e "s/\.*<\/Host>.*$/<Valve className=\"org.apache.catalina.valves.RemoteIpValve\" protocolHeader=\"x-forwarded-proto\" \/><\/Host>/" /opt/TeamCity/conf/server.xml
+
+# Copy buildAgent 
+RUN cp -r /opt/TeamCity/buildAgent /opt && \
+    chmod +x /opt/buildAgent/bin/agent.sh
 
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,5 +11,15 @@ fi
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 git config --global http.sslVerify false
 
+# Install Agent Custom Token Authorize
+wget -P $TEAMCITY_DATA_PATH/plugins https://teamcity.jetbrains.com/guestAuth/repository/download/TeamCityPluginsByJetBrains_AgentAutoAuthorize_90x/394696:id/agentMagicAuthorize.zip
+
+echo -e "agent.authorize.tokens=gantry_crane:'VVyS!w@0cUXf*R!!UVvjPt9#abY7m#*M8vk$SLUGtPsHgH3aVi':1:0,firefox_esr:'kla^Q59hznR8AvT$ziY!Co3wVbH6mQqnfF9ywrnAP5$w&Rkkz@'1:1:1" > $TEAMCITY_DATA_PATH/config/internal.properties
+
+echo -e "serverUrl=127.0.0.1:8111\nname=gantry_crane\nteamcity.magic.authorizationToken='VVyS!w@0cUXf*R!!UVvjPt9#abY7m#*M8vk$SLUGtPsHgH3aVi'" > /opt/TeamCity/buildAgent/conf/buildAgent.properties
+
 echo "Starting teamcity..."
 exec /opt/TeamCity/bin/teamcity-server.sh run
+
+echo "Starting buildAgent..."
+exec /opt/TeamCity/buildAgent/bin/agent.sh run &
